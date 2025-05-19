e16690fe49609e8368f4a22a4fcf2b623c11f26c
alexandre pinto <alexandrepinto>
2025-05-18 21:05:09 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); // Chama o widget raiz MyApp()
+  int valor = 10;
+
+  runApp(MyApp(title: "ola mundo", valor: valor)); // Chama o widget raiz MyApp()
+  valor++;
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String title;
+  final int valor;
+
+  const MyApp({Key? key, this.title = "", this.valor = 0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue[100],
-        appBar: AppBar(title: Text("aplicativo olá mundo")),
+        appBar: AppBar(title: Text(this.title)),
         body: Center(
           child: Text(
-            "ola mundo",
-            style: TextStyle(fontSize: 50, color: Colors.black),
+            "ola mundo, valor é ${this.valor}",
+            style: TextStyle(fontSize: 30, color: Colors.black),
           ),
         ),
       ),
